a7b1f3383a15033cba666f1c7aa2015cb1c9770a
Lucas Ortigoso <lucas.ortigoso@gmail.com>
2018-12-12 01:12:35 -0200

    no message

--- test.js
+++ test.js
@@ -7,7 +7,7 @@ var palette = require('./')
   , canvas = new Canvas
   , ctx = canvas.getContext('2d')
   , path = process.argv[2]
-  , n = ~~process.argv[3] || 20
+  , n = ~~process.argv[3] || 5
   , out = '/tmp/out.png';
 
 if (!path) {
